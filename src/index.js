9999897914aca474289c73b78244487f025eb943
arifrashidi <78742458+arifrashidi@users.noreply.github.com>
2023-07-14 16:22:57 +0800

    commit 5

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 
-function openMenu(){
-    document.body.classList.add("menu_open")
+//🔸 Modal window function
 
-}
+// ^ querySelector 
+const nav_open_window_button = document.querySelector(".nav_open_window_button");
+const nav_closed_window_button = document.querySelectorAll(".nav_closed_window_button");
 
-function closeMenu() {
-    document.body.classList.remove('menu_open')
-}
+// ^ addEventListener 
+nav_open_window_button.addEventListener("click", function() {
+    document.body.classList.add("js-when-button-been-clicked")
+});
+
+nav_closed_window_button.forEach(el => {
+    el.addEventListener('click', function() {
+        document.body.classList.remove('js-when-button-been-clicked')
+        console.log("remove");
+    });
+});
+
+console.log("Thanks for visiting");
